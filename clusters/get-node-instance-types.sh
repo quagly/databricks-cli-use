fe27c357b5c90c2663d1658b4b8083a79962fd6c
Michael West <quagly@gmail.com>
2018-07-03 17:20:38 +0000

    get node instance types

---
+++ clusters/get-node-instance-types.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# get all supported databricks node instance types
+# exclude deprecated instances
+INSTANCE_TYPES=$(databricks clusters list-node-types  | jq ".node_types[] | select(.is_deprecated==false) | .instance_type_id" | tr -d '"' | sort -u)
+echo "${INSTANCE_TYPES}"
+
